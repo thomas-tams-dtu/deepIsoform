ea504fb0a77baf7983c0892374293f7a43f36931
thomas-tams-dtu <s204540@dtu.dk>
2023-11-20 13:45:23 +0100

    finding explained variance from different models

--- tmp/explained_var.py
+++ tmp/explained_var.py
@@ -2,9 +2,10 @@
 
 import pickle
 
-LATENT_FEATURES = 32
+LATENT_FEATURES = 256
 
 IPCA = f'/zhome/99/d/155947/DeeplearningProject/deepIsoform/models/ipca_model_n{LATENT_FEATURES}.pkl'
+print(IPCA)
 
 with open(IPCA, 'rb') as file:
     ipca = pickle.load(file)
