e2c072a31f3b138d00e14215c4b18834bea7103f
thomas-tams-dtu <s204540@dtu.dk>
2023-11-20 13:41:28 +0100

    added different size networks and arguments

--- scripts/FFNN.py
+++ scripts/FFNN.py
@@ -3,7 +3,7 @@ from torch import nn
 import numpy as np
 from typing import *
 
-class FeedForwardIsoform(nn.Module):
+class FeedForwardIsoform_small(nn.Module):
     """A Variational Autoencoder with
     * a Bernoulli observation model `p_\theta(x | z) = B(x | g_\theta(z))`
     * a Gaussian prior `p(z) = N(z | 0, I)`
@@ -11,7 +11,46 @@ class FeedForwardIsoform(nn.Module):
     """
 
     def __init__(self, input_shape:torch.Size, output_shape:torch.Size) -> None:
-        super(FeedForwardIsoform, self).__init__()
+        super(FeedForwardIsoform_small, self).__init__()
+
+        self.input_shape = input_shape
+        self.output_shape = output_shape
+        self.input_features = np.prod(input_shape)
+        self.output_features = np.prod(output_shape)
+
+        self.FNN = nn.Sequential(
+            nn.Linear(in_features=self.input_features, out_features=512),
+            nn.ReLU(),
+            nn.BatchNorm1d(512),
+            nn.Dropout(p=0.5),
+            nn.Linear(in_features=512, out_features=1024),
+            nn.ReLU(),
+            nn.BatchNorm1d(1024),
+            nn.Linear(in_features=1024, out_features=self.output_features)
+        )
+
+        for layer in self.FNN:
+            if isinstance(layer, torch.nn.Linear):
+                torch.nn.init.xavier_uniform_(layer.weight)
+                #for param in layer.parameters():
+                #    print(f"Parameter dtype: {param.dtype}")
+
+    def forward(self, x) -> Dict[str, Any]:
+        # flatten the input
+        x = x.view(x.size(0), -1)
+        x = self.FNN(x)
+
+        return x
+
+class FeedForwardIsoform_medium(nn.Module):
+    """A Variational Autoencoder with
+    * a Bernoulli observation model `p_\theta(x | z) = B(x | g_\theta(z))`
+    * a Gaussian prior `p(z) = N(z | 0, I)`
+    * a Gaussian posterior `q_\phi(z|x) = N(z | \mu(x), \sigma(x))`
+    """
+
+    def __init__(self, input_shape:torch.Size, output_shape:torch.Size) -> None:
+        super(FeedForwardIsoform_medium, self).__init__()
 
         self.input_shape = input_shape
         self.output_shape = output_shape
@@ -32,6 +71,50 @@ class FeedForwardIsoform(nn.Module):
             nn.BatchNorm1d(1024),
             nn.Dropout(p=0.25),
             nn.Linear(in_features=1024, out_features=self.output_features)
+        )
+
+        for layer in self.FNN:
+            if isinstance(layer, torch.nn.Linear):
+                torch.nn.init.xavier_uniform_(layer.weight)
+                #for param in layer.parameters():
+                #    print(f"Parameter dtype: {param.dtype}")
+
+    def forward(self, x) -> Dict[str, Any]:
+        # flatten the input
+        x = x.view(x.size(0), -1)
+        x = self.FNN(x)
+
+        return x
+
+class FeedForwardIsoform_large(nn.Module):
+    """A Variational Autoencoder with
+    * a Bernoulli observation model `p_\theta(x | z) = B(x | g_\theta(z))`
+    * a Gaussian prior `p(z) = N(z | 0, I)`
+    * a Gaussian posterior `q_\phi(z|x) = N(z | \mu(x), \sigma(x))`
+    """
+
+    def __init__(self, input_shape:torch.Size, output_shape:torch.Size) -> None:
+        super(FeedForwardIsoform_large, self).__init__()
+
+        self.input_shape = input_shape
+        self.output_shape = output_shape
+        self.input_features = np.prod(input_shape)
+        self.output_features = np.prod(output_shape)
+
+        self.FNN = nn.Sequential(
+            nn.Linear(in_features=self.input_features, out_features=1024),
+            nn.ReLU(),
+            nn.BatchNorm1d(1024),
+            nn.Dropout(p=0.5),
+            nn.Linear(in_features=1024, out_features=2048),
+            nn.ReLU(),
+            nn.BatchNorm1d(2048),
+            nn.Dropout(p=0.25),
+            nn.Linear(in_features=2048, out_features=2048),
+            nn.ReLU(),
+            nn.BatchNorm1d(2048),
+            nn.Dropout(p=0.25),
+            nn.Linear(in_features=2048, out_features=self.output_features)
         )
 
         for layer in self.FNN:
--- scripts/dense_train.py
+++ scripts/dense_train.py
@@ -6,36 +6,54 @@ from torch.utils.data import DataLoader
 from tqdm import tqdm
 import numpy as np
 from typing import *
-from FFNN import FeedForwardIsoform
+from FFNN import FeedForwardIsoform_small, FeedForwardIsoform_medium, FeedForwardIsoform_large
+from write_training_data import write_training_data
 from collections import defaultdict
 import pickle
 from plot_loss import plot_loss
+import argparse
+import sys
+
+# Arguments
+parser = argparse.ArgumentParser(description='Training dense neural network')
+parser.add_argument('-ns', type=str, help='Network size. Choose between small, medium, large')
+parser.add_argument('-lf', type=int, help='Latents features used for encoding. Choose between 16, 32, 64, 128, 256')
+parser.add_argument('-wd', type=float, help='Weight decay used for Adam optimizer')
+parser.add_argument('--sm', action='store_true', help='Save model')
+args = parser.parse_args()
 
 
-def cross_entropy(ys, ts):
-    # computing cross entropy per sample
-    cross_entropy = -torch.sum(ts * torch.log(ys), dim=1, keepdim=False)
-    # averaging over samples
-    return torch.mean(cross_entropy)
-
-def accuracy(ys, ts):
-    # making a one-hot encoded vector of correct (1) and incorrect (0) predictions
-    correct_prediction = torch.eq(torch.max(ys, 1)[1], torch.max(ts, 1)[1])
-    # averaging the one-hot encoded vector
-    return torch.mean(correct_prediction.float())
-
-
+# Initialize training parameters
+NETWORK_SIZE = args.ns
+SAVE_MODEL = args.sm
+LATENT_FEATURES = args.lf
 BATCH_SIZE = 500
-LATENT_FEATURES = 32
 LEARNING_RATE = 1e-4
-WEIGHT_DECAY = 1e-5
+WEIGHT_DECAY = args.wd   # 1e-5
 NUM_EPOCHS = 100
 PATIENCE = 6
-MODEL_NAME = f'PCA_DENSE_l{LATENT_FEATURES}_lr{LEARNING_RATE}_e{NUM_EPOCHS}_wd{WEIGHT_DECAY}_p{PATIENCE}_dout_bn_medium+'
+MODEL_NAME = f'PCA_DENSE_l{LATENT_FEATURES}_lr{LEARNING_RATE}_e{NUM_EPOCHS}_wd{WEIGHT_DECAY}_p{PATIENCE}'
 IPCA = f'/zhome/99/d/155947/DeeplearningProject/deepIsoform/models/ipca_model_n{LATENT_FEATURES}.pkl'
+METADATA_SAVE_PATH = f'/zhome/99/d/155947/DeeplearningProject/deepIsoform/data/training_meta_data/train_data_{NETWORK_SIZE}.tsv'
+PLOT_PATH = f'/zhome/99/d/155947/DeeplearningProject/deepIsoform/model_plots/dense_train/{MODEL_NAME}_loss_plot.png'
+MODEL_PATH = f'/zhome/99/d/155947/DeeplearningProject/deepIsoform/data/bhole_storage/models/{MODEL_NAME}'
 
+# Check if size is proper
+if NETWORK_SIZE not in ['small', 'medium', 'large']:
+    print('Network size has to be either small, medium or large. Got', f'\'{NETWORK_SIZE}\'')
+    sys.exit(1)
+
+# Check if latent features are proper
+if LATENT_FEATURES not in [16, 32, 64, 128, 256]:
+    print(f'Number of latents features has to be 16, 32, 64, 128, 256. Got \'{LATENT_FEATURES}\'')
+    sys.exit(1)
+
+# Printing some data to std out
 print('model name', MODEL_NAME)
+print('PCA model', IPCA)
 
+
+### INIT DATASET
 # Load gtex datasets
 gtex_train = IsoDatasets.GtexDataset("/dtu-compute/datasets/iso_02456/hdf5/", exclude='brain')
 gtex_test = IsoDatasets.GtexDataset("/dtu-compute/datasets/iso_02456/hdf5/", include='brain')
@@ -46,17 +64,29 @@ print("gtex test set size:", len(gtex_test))
 gtx_train_dataloader = DataLoader(gtex_train, batch_size=BATCH_SIZE, shuffle=True)
 gtx_test_dataloader = DataLoader(gtex_test, batch_size=BATCH_SIZE, shuffle=True)
 
+### INIT PCA MODEL
 # Define PCA model
 with open(IPCA, 'rb') as file:
     ipca = pickle.load(file)
 
-# Define the Dense neural network
+### INIT FNN
+# Grab a sample to initialize latent features and output size for network
 gene_expr, isoform_expr = next(iter(gtx_train_dataloader))
-FNN = FeedForwardIsoform(input_shape = LATENT_FEATURES, 
-                         output_shape = isoform_expr[0].size())
+
+# Select corresponding network
+if NETWORK_SIZE == 'small':
+    FNN = FeedForwardIsoform_small(input_shape = LATENT_FEATURES, 
+                             output_shape = isoform_expr[0].size())
+elif NETWORK_SIZE == 'medium':
+    FNN = FeedForwardIsoform_medium(input_shape = LATENT_FEATURES, 
+                             output_shape = isoform_expr[0].size())
+elif NETWORK_SIZE == 'large':
+    FNN = FeedForwardIsoform_large(input_shape = LATENT_FEATURES, 
+                             output_shape = isoform_expr[0].size())
 
 print(FNN)
 
+### INIT TRAINING 
 # Loss function
 criterion = torch.nn.MSELoss()
 
@@ -66,31 +96,27 @@ optimizer = torch.optim.Adam(FNN.parameters(), lr=LEARNING_RATE, weight_decay=WE
 # Set up learning rate scheduler
 #scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.5)  # Reduce LR by a factor of 0.5 every 10 epochs
 
-# define dictionary to store the training curves
-training_data =   [] #defaultdict(list)
-validation_data = [] #defaultdict(list)
-
+# Define list to store the loss
+training_loss =   []
+validation_loss = []
 
 # If GPU available send to gpu
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 print(f">> Using device: {device}")
 
-# move the model to the device
 FNN = FNN.to(device)
 criterion = criterion.to(device)
 
-
+### TRAINING ...
 best_val_loss = float('inf')
-# training..
 epoch = 0
 while epoch < NUM_EPOCHS:
     epoch+= 1
     print('Training epoch', epoch)
-    training_epoch_data = [] #[defaultdict(list)]
+    training_epoch_data = []
     FNN.train()
 
     # Go through each batch in the training dataset using the loader
-    # Note that y is not necessarily known as it is here
     for x, y in tqdm(gtx_train_dataloader):
         # Send to device and do PCA
         x = ipca.transform(x)
@@ -109,7 +135,7 @@ while epoch < NUM_EPOCHS:
 
         training_epoch_data.append(loss.mean().item())
 
-    training_data.append(np.mean(training_epoch_data))
+    training_loss.append(np.mean(training_epoch_data))
 
     # Evaluate on a single batch, do not propagate gradients
     with torch.no_grad():
@@ -129,11 +155,11 @@ while epoch < NUM_EPOCHS:
         # Calculate loss
         loss = criterion(x, y)
 
-        validation_data.append(loss.mean().item())
+        validation_loss.append(loss.mean().item())
     
-    # Primitive early stopping
-    if validation_data[-1] < best_val_loss:
-        best_val_loss = validation_data[-1]
+    # Early stopping
+    if validation_loss[-1] < best_val_loss:
+        best_val_loss = validation_loss[-1]
         early_stopping_counter = 0
     else:
         early_stopping_counter += 1
@@ -143,35 +169,44 @@ while epoch < NUM_EPOCHS:
         print(f"Early stopping! Training stopped at epoch {epoch}.")
         break
 
-### Plotting val and train data
-plot_path = f'/zhome/99/d/155947/DeeplearningProject/deepIsoform/model_plots/dense_train/{MODEL_NAME}_loss_plot.png'
-plot_loss(training_loss=training_data, validation_loss=validation_data, save_path=plot_path)
+### PLOTTING, SAVING METADATA FROM TRAINING AND MODEL
+# Plotting val and train data
+plot_loss(training_loss=training_loss, validation_loss=validation_loss, save_path=PLOT_PATH)
 
+# Saving model training metadata
+write_training_data(file_path=METADATA_SAVE_PATH,
+                    network_name=MODEL_NAME,
+                    network_size=NETWORK_SIZE,
+                    latent_features=LATENT_FEATURES,
+                    learning_rate=LEARNING_RATE,
+                    weight_decay=WEIGHT_DECAY,
+                    patience=PATIENCE,
+                    training_runs=epoch,
+                    train_loss= training_loss,
+                    eval_loss=  validation_loss)
 
-### Saving model
-# Save path for model
-model_path = f'/zhome/99/d/155947/DeeplearningProject/deepIsoform/data/bhole_storage/models/{MODEL_NAME}'
+# Saving model
+if SAVE_MODEL:
+    init_values = {'batch_size': BATCH_SIZE,
+                   'num_epochs': NUM_EPOCHS}
 
-init_values = {'batch_size': BATCH_SIZE,
-               'num_epochs': NUM_EPOCHS}
+    layer_sizes = [(layer.in_features, layer.out_features) for layer in FNN.FNN if isinstance(layer, torch.nn.Linear)]
 
-layer_sizes = [(layer.in_features, layer.out_features) for layer in FNN.FNN if isinstance(layer, torch.nn.Linear)]
+    # Create a dictionary to save additional information (optional)
+    info = {
+        'architecture': MODEL_NAME ,
+        'init_values': init_values,
+        'hyperparameters': {
+            'learning_rate': LEARNING_RATE,
+            'weight_decay': WEIGHT_DECAY,
+            'layer_size': layer_sizes
+        },
+        'patience': PATIENCE,
+        'num_epochs': epoch,
+        'train_loss': training_loss,
+        'validation_loss': validation_loss
+    }
 
-# Create a dictionary to save additional information (optional)
-info = {
-    'architecture': MODEL_NAME ,
-    'init_values': init_values,
-    'hyperparameters': {
-        'learning_rate': LEARNING_RATE,
-        'weight_decay': WEIGHT_DECAY,
-        'layer_size': layer_sizes
-    },
-    'patience': PATIENCE,
-    'num_epochs': epoch,
-    'train_loss': training_data,
-    'validation_loss': validation_data
-}
-
-# Save the model and additional information
-torch.save({'model_state_dict': FNN.state_dict(), 'info': info,},
-            model_path)
+    # Save the model and additional information
+    torch.save({'model_state_dict': FNN.state_dict(), 'info': info},
+                MODEL_PATH)
