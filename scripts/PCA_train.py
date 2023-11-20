6e6b93311eec9041d038a948e9ad0154dc2e918e
thomas-tams-dtu <s204540@dtu.dk>
2023-11-20 13:44:07 +0100

    used for training pca models

--- scripts/PCA_train.py
+++ scripts/PCA_train.py
@@ -6,10 +6,14 @@ from sklearn.decomposition import IncrementalPCA
 from tqdm import tqdm
 import pickle
 
-N_COMPONENTS = 32
+N_COMPONENTS = 64
+
+
+save_path = f'/zhome/99/d/155947/DeeplearningProject/deepIsoform/models/ipca_model_n{N_COMPONENTS}.pkl'
+print(save_path)
 
 archs4_train = IsoDatasets.Archs4GeneExpressionDataset("/dtu-compute/datasets/iso_02456/hdf5/")
-archs4_train_dataloader = DataLoader(archs4_train, batch_size=100, shuffle=True)
+archs4_train_dataloader = DataLoader(archs4_train, batch_size=500, shuffle=True)
 
 ipca = IncrementalPCA(n_components=N_COMPONENTS)
 
@@ -19,5 +23,5 @@ for X in tqdm(archs4_train_dataloader):
     ipca.partial_fit(X.numpy())
 
 # Save model
-with open(f'/zhome/99/d/155947/DeeplearningProject/deepIsoform/models/ipca_model_n{N_COMPONENTS}_small.pkl', 'wb') as file:
+with open(save_path, 'wb') as file:
     pickle.dump(ipca, file)
