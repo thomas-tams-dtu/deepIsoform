f23d11f6a52d6d6823149ccc3d2c421393d873d3
thomas-tams-dtu <s204540@dtu.dk>
2023-12-17 17:23:50 +0100

    fix: use same train/val/test split as other models

--- scripts/train_baselineMean.py
+++ scripts/train_baselineMean.py
@@ -7,8 +7,9 @@ from tqdm import tqdm
 
 # Example of making a training set that excludes samples from the brain and a test set with only samples from the brain
 # If you have enough memory, you can load the dataset to memory using the argument load_in_mem=True
-gtex_train = IsoDatasets.GtexDataset("/dtu-compute/datasets/iso_02456/hdf5/", exclude='brain')
-gtex_test = IsoDatasets.GtexDataset("/dtu-compute/datasets/iso_02456/hdf5/", include='brain')
+gtex_train = IsoDatasets.GtexDataset("/dtu-compute/datasets/iso_02456/hdf5-row-sorted/", exclude=['brain', 'Artery'])
+gtex_val = IsoDatasets.GtexDataset("/dtu-compute/datasets/iso_02456/hdf5-row-sorted/", include='brain')
+gtex_test = IsoDatasets.GtexDataset("/dtu-compute/datasets/iso_02456/hdf5-row-sorted/", include='Artery')
 
 print("gtex training set size:", len(gtex_train))
 print("gtex test set size:", len(gtex_test))
@@ -17,8 +18,11 @@ print(len(gtex_test)/(len(gtex_test)+len(gtex_train)) * 100)
 
 gtx_train_dataloader = DataLoader(gtex_train, batch_size=5000, shuffle=True)
 gtx_test_dataloader = DataLoader(gtex_test, batch_size=5000, shuffle=True)
+
+
+print('Training means for each attribute')
 first = True
-for X,y in gtx_train_dataloader:
+for X, y, _ in tqdm(gtx_train_dataloader):
     
     if first:
         a = torch.sum(y, dim=0)
@@ -30,10 +34,11 @@ for X,y in gtx_train_dataloader:
 meanTensor = torch.div(a,len(gtex_train))
 
 
+print('Calculating distance to test')
 sum_squared_error = 0
 large = 0
 totalLength = 0
-for X, y in gtx_test_dataloader:
+for X, y, _ in gtx_test_dataloader:
     predictions = meanTensor.expand_as(y)
     
     squared_errors =(predictions - y)**2
