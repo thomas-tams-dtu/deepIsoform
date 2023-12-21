4a737ec550cc561e02e5e62a46e3b1518f71c2f9
thomas-tams-dtu <s204540@dtu.dk>
2023-12-21 15:29:31 +0100

    fix: loops

--- scripts/train_baselineMean_archs4.py
+++ scripts/train_baselineMean_archs4.py
@@ -11,7 +11,7 @@ archs4_dataset_train = IsoDatasets.Archs4GeneExpressionDataset('/dtu-compute/dat
 archs4_dataset_val = IsoDatasets.Archs4GeneExpressionDataset('/dtu-compute/datasets/iso_02456/hdf5-row-sorted/',
                                                              validation_set=True)
 print("archs4 training set size:", len(archs4_dataset_train))
-print("archs4 test set size:", len(archs4_dataset_val))
+print("archs4 validation set size:", len(archs4_dataset_val))
 
 archs4_train_dataloader = DataLoader(archs4_dataset_train, batch_size=500, shuffle=True)
 archs4_val_dataloader = DataLoader(archs4_dataset_val, batch_size=1, shuffle=True)
@@ -19,14 +19,14 @@ archs4_val_dataloader = DataLoader(archs4_dataset_val, batch_size=1, shuffle=Tru
 
 print('Training means for each attribute')
 first = True
-for X, y, _ in tqdm(archs4_train_dataloader):
+for X in tqdm(archs4_train_dataloader):
     
     if first:
-        a = torch.sum(y, dim=0)
+        a = torch.sum(X, dim=0)
         first = False
         continue
     
-    a = torch.add((torch.sum(y, dim=0)), a)
+    a = torch.add((torch.sum(X, dim=0)), a)
 
 meanTensor = torch.div(a,len(archs4_dataset_train))
 print(meanTensor)
@@ -35,13 +35,10 @@ print(meanTensor.size())
 
 print('Calculating distance to validation set')
 sum_squared_error = 0
-totalLength = 0
-for X, y, _ in tqdm(archs4_val_dataloader):
-    predictions = meanTensor.expand_as(y)
+for X in tqdm(archs4_val_dataloader):
+    predictions = meanTensor.expand_as(X)
     
-    squared_errors =(predictions - y)**2
-    
-    totalLength += len(X)
+    squared_errors =(predictions - X)**2
     
     sum_squared_error += squared_errors.sum()
 
@@ -50,5 +47,4 @@ mse_loss_val = sum_squared_error / (len(archs4_dataset_val) * 156958)
 
 
 #Print the MSE loss
-print("Total length:" ,totalLength)
 print(f'MSE Val Loss: {mse_loss_val.item()}')
